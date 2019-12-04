cf9f12e3680e8fb7149604238badfbd74f2f5ad7
Vitaliy <vitalijbukatkin@vitaliy.ghostbsd-pc.home>
2019-12-04 12:55:46 +0300

    Initial commit with start.spring.io

---
+++ src/test/java/com/github/witalijbukatkin/PhoneBook/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.witalijbukatkin.PhoneBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
